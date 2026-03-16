f48a808e0be4b388b35ca2998aa44a2177166268
adham-samin00 <adhamsamin0@gmail.com>
2026-03-16 23:06:30 +0600

    js validation (have some issues)

---
+++ LabPerformance_Mid/JS/Validation.js
@@ -0,0 +1,47 @@
+console.log("Connected");
+
+let unitPrice = 1000;
+let days = 30;
+
+function getQuantity() 
+{
+    let quantity = document.getElementById("quantity").value;
+
+    if(quantity === "" || quantity < 0)
+    {
+        quantity = 0;
+        alert("You should select a valid quantity.");
+    }
+
+    return quantity;
+}
+
+function calculateTotal(quantity) 
+{
+    let total = unitPrice * quantity * days;
+    return total;
+}
+
+function displayTotal(total)
+{
+    document.getElementById("totalPrice").value = total;
+}
+
+function checkCoupon(total)
+{
+    if(total > 1000)
+    {
+        alert("You are eligible for a gift coupon.");
+    }
+}
+
+function collect_data()
+{
+    let quantity = getQuantity();
+
+    let total = calculateTotal(quantity);
+
+    displayTotal(total);
+
+    checkCoupon(total); 
+}
