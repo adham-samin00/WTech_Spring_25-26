cf189a80ff809ad55494423fcae1de0169b99c30
adham-samin00 <adhamsamin0@gmail.com>
2026-03-25 15:48:12 +0600

    JS characters updated

---
+++ PracticeJS/JS/validation.js
@@ -0,0 +1,16 @@
+function analyzeText()
+{
+    let text = document.getElementById("inputText").value;
+    let result = document.getElementById("result");
+
+    let charCount = text.length;
+
+    let word = text.split(" ");
+    let wordCount = word.length;
+
+
+
+
+    result.innerHTML = 
+    "Number of characters: " + charCount;
+}
