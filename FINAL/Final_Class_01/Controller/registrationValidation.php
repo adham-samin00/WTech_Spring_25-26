ef3512261ad3b2cb77446b221b5404b34df63874
adham-samin00 <adhamsamin0@gmail.com>
2026-04-14 21:12:58 +0600

    upto email validation

---
+++ FINAL/Final_Class_01/Controller/registrationValidation.php
@@ -0,0 +1,50 @@
+<?php
+
+$name = "";
+$email = "";
+$address = "";
+$gender = "";
+
+$nameErr = "";
+$emailErr = "";
+$addressErr = "";
+$genderErr = "";
+
+
+
+if($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $address = $_POST["address"];
+        $gender = isset($_POST["gender"]) ? $_POST["gender"] : "";
+    
+
+
+        if(empty($name))
+            {
+                $nameErr = "Name field cannot be empty";
+            }
+        else if(!preg_match("/^[a-zA-Z-' ]*$/", $name))
+            {
+                $nameErr = "Only letters and whitespaces are allowed";
+            }
+        
+        if(empty($email))
+            {
+                $emailErr = "Email is required";
+            }
+        else if(!preg_match("/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/", $email))
+            {
+                $emailErr = "Invalid email format";
+            }
+
+
+    }
+
+
+
+
+
+
+?>
---
+++ FINAL/Final_Class_01/View/registration.php
@@ -0,0 +1,56 @@
+<?php
+include "../Controller/registrationValidation.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>PHP Validation Example</title>
+</head>
+
+<body>
+    <form method = "post" action = "">
+        <table>
+            <tr>
+                <td><p style="color: red;">* required field</p></td>
+            </tr>
+
+            <tr>
+                <td> <label for = "name"> Name: </label> </td>
+                <td> 
+                    <input type = "text" id = "name" name = "name"> <?php echo $name ?> 
+                    <p style = "color:red"> <?php echo $nameErr ?> </p>
+                </td> 
+                <td> <p style = "color:red">*</p> </td>
+            </tr>
+            
+            <tr>
+                <td> <label for = "email"> Email: </label> </td>
+                <td> 
+                    <input type = "text" id = "email" name = "email"> <?php echo $email?> 
+                    <p style = "color:red"> <?php echo $emailErr ?> </p>
+                </td>
+                <td> <p style = "color:red">*</p> </td>
+            </tr>
+
+
+            <tr>
+                <td> <label for = "address"> Address: </label> </td>
+                <td> <textarea id = "address" name = "address" rows = "4" cols = "30"> </textarea> <?php echo $address ?></td>
+                <td> <p style = "color:red">*</p> </td>
+            </tr>
+            
+
+            <tr>
+                <td> <label for = "gender"> Gender: </label> </td>
+                <td> <input type = "radio" id = "male" name = "gender"> <label for = "male">Male</label> </td>
+                <td> <input type = "radio" id = "female" name = "gender"> <label for = "female">Female</label> <?php echo $gender ?> </td>
+                <td> <p style = "color:red">*</p> </td>
+            </tr>
+
+            <tr>
+                <td> <input type = "submit" id = "submit" name = "submit"> </td>
+            </tr>
+
+        </table>
+    </form> 
